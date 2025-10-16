c76743e39285749e5dab4b319d1a528105cd9327
BLopezComputec <blopez@computec.com>
2025-10-15 21:16:14 -0500

    update docker

--- src/main/java/com/computec/convertirpdftohtm/service/Pdf2HtmlExService.java
+++ src/main/java/com/computec/convertirpdftohtm/service/Pdf2HtmlExService.java
@@ -130,10 +130,6 @@ public class Pdf2HtmlExService {
         // No agregues "pdf2htmlEX": es el ENTRYPOINT de la imagen
 
         cmd.add("--zoom"); cmd.add(zoom);
-        cmd.add("--split-pages"); cmd.add("0");
-        cmd.add("--embed-css"); cmd.add("1");
-        cmd.add("--embed-font"); cmd.add("1");
-        cmd.add("--embed-image"); cmd.add("1");
         cmd.add(workDir.relativize(pdf).toString());
         cmd.add(outHtml.getFileName().toString());
 
